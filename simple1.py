39c9bd842001ecc78eb3f4dd10efb42a70f72660
Ostap Kostiuk <164146945+sodaqqq@users.noreply.github.com>
2025-04-02 14:04:29 +0300

    Delete simple1.py

--- simple1.py
+++
@@ -1,9 +0,0 @@
-def stringify(node):
-    result = []
-
-    while node:
-        result.append(str(node.data))
-        node = node.next
-    result.append("None")
-
-    return " -> ".join(result)
